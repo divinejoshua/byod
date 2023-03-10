eb61172a2bc6c18709d5f0749618cd5c60a15ecb
divinejoshua <divineuche399@gmail.com>
2023-03-10 15:45:36 +0000

    Added homeview

--- contextapp/views.py
+++ contextapp/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
 # Create your views here.
+# Create your views here.
+class HomeView(TemplateView):
+    template_name = "contextapp/index.html"
+    context = {}
+
+    # Get request 
+    def get(self, request, *args, **kwargs):
+        return render(request, self.template_name)
